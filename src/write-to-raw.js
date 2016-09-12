b9b6908146a4053eec6f81c4e04ed28b930cfa65
Maxime Robin <contact.waxo@gmail.com>
2016-09-12 13:59:29 +0200

    Update arrayToRaw for better comprehension and better accuracy

--- src/write-to-raw.js
+++ src/write-to-raw.js
@@ -9,11 +9,11 @@ const fs = require('fs');
  */
 const arrayToRaw = (array, outputName, outputPath = '') => {
   const output = (outputPath) ? `${outputPath}${outputName}` : outputName;
-  const buff = new Buffer(array.length * array[0].length * 4);
-  array.forEach((frame, idx) => {
-    frame.forEach((value, index) => {
-      buff.writeFloatLE(value, ((idx * frame.length) + index) * 4);
-    });
+  console.log(array[0].length);
+  const arr = new Float32Array(array.reduce((a, b) => a.concat(b)));
+  const buff = new Buffer(arr.length * 4);
+  arr.forEach((value, idx) => {
+    buff.writeFloatLE(value, idx * 4);
   });
   const wStream = fs.createWriteStream(output);
   wStream.write(buff);
